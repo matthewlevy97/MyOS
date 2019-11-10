d1f9f3c96a317e4983f160df77ecc9ccf3477d5e
matthewlevy97 <mrl220@lehigh.edu>
2019-11-09 22:30:41 -0500

    Updated boot.S to use Multiboot 2
    Qemu does NOT support multiboot 2 for the '-kernel' flag. To bypass
    	this, Makefile was updated to build an ISO and run the ISO.
    	This is making the sysroot directory more useful and should
    	allow booting of the kernel in other emulators / hardware (possibly).

--- kernel/src/init.c
+++ kernel/src/init.c
@@ -1,20 +1,39 @@
-#include <kernel/kprint.h>
-#include <kernel/timer.h>
-#include <kernel/serial.h>
-#include <kernel/kmalloc.h>
-#include <kernel/i686/pic.h>
-#include <kernel/i686/descriptor_tables.h>
 #include <macros.h>
+#include <kernel/kprint.h>
+#include <kernel/kmalloc.h>
+#include <kernel/kpanic.h>
+#include <kernel/multiboot.h>
+#include <kernel/serial.h>
+#include <kernel/timer.h>
+#include <kernel/i686/descriptor_tables.h>
+#include <kernel/i686/pic.h>
 
 /**
  * @brief      Entry point into kernel for C code
  */
-void FUNCTION_NO_RETURN kinit()
+void FUNCTION_NO_RETURN kinit(struct multiboot_tag * mb_header, uint32_t mb_magic)
 {
 	// Initialization
 	kprint_init();
 	serial_init();
 	enable_serial_output();
+
+	// Ensure everything is good
+	if(mb_magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+		kprintf(KPRINT_ERROR "Not loaded with Multiboot 2!\n");
+		kpanic();
+	}
+
+	/**
+	 * Parse the multiboot header.
+	 * The header is in the following format:
+	 * 			+-------------------+
+	 *	u32     | total_size        |
+	 *	u32     | reserved          |
+     *   	 	+-------------------+
+	 * We cheat and use the multiboot_tag struct here. Ignore names
+	 */
+	kprintf(KPRINT_DEBUG "Multiboot Header Length: 0x%x\n", mb_header->type);
 
 	pic_init();
 	kprintf(KPRINT_DEBUG "PIC Initialized\n");
