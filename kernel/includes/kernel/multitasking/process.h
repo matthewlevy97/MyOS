a896b41bef142ba482bf70b697bce8c1e29e9cef
matthewlevy97 <mrl220@lehigh.edu>
2020-01-08 20:00:54 -0500

    Added documentation headers to structures.
    Added TODOs in switch.S

--- kernel/includes/kernel/multitasking/process.h
+++ kernel/includes/kernel/multitasking/process.h
@@ -5,23 +5,43 @@
 
 #define PROCESS_STACK_BASE_ADDRESS 0x7FFFFFFF
 
+/**
+ * @brief      Flags for creation of a new process
+ */
 enum process_creation_flags {
 	NO_CREATE_STACK = 0x01
 };
 
+/**
+ * Typedef for the PID of a process
+ */
 typedef uint32_t pid_t;
 
+/**
+ * @brief      Register information for a process.
+ * 
+ * DO NOT MODIFY. Any changes need to be reflected in switch.S
+ */
 struct process_registers {
 	uint32_t eax, ebx, ecx, edx, esi, edi, esp, ebp, eip, eflags, cr3;
 };
 
+/**
+ * @brief      Stores information about a process
+ */
 struct process_control_block {
+	/* Update switch.S if modified */
 	struct process_registers registers;
 	uintptr_t tss_esp0;
+	/* Can modify below this point freely */
+	
 	struct process_control_block *next;
 	pid_t pid;
 };
 
+/**
+ * @brief      Holds a pointer to the currently running process
+ */
 extern struct process_control_block *current_process;
 
 void process_init();
